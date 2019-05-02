c5a0aaf19cc75fe73d3e1c4ce8d1dd655d9656bb
Edward Turner <edward.turnerr@gmail.com>
2019-05-02 18:39:26 -0400

    edited class

--- scala-package/src/main/scala/com/lal/Weights.scala
+++ scala-package/src/main/scala/com/lal/Weights.scala
@@ -1,11 +1,17 @@
 package com.lal
 
-abstract class Weights(featureCol: String, predictionCol: String, metricName: String) {
+import org.apache.spark.ml.param.Param
+import org.apache.spark.ml.tuning.{ParamGridBuilder, TrainValidationSplit}
 
-  val space: Any
+abstract private class Weights(featureCol: String, predictionCol: String, metricName: String) {
+
+  type Space = Map[Param[_], Iterable[_]]
+
+  val paramgrid: Space
 
   def optModel: Any
 
   def getFeatureWeights: Any
 
 }
+
