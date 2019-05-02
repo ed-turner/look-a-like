190f06f5d810820b39af5efabfee5440d2e61fd0
Edward Turner <edward.turnerr@gmail.com>
2019-05-02 11:33:53 -0400

    setup setup.py

---
+++ python-package/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+with open("requirements.txt", 'r') as f:
+    REQUIREMENTS = f.read()
+
+with open("README.md") as f:
+    README = f.readlines()
+
+LICENSE = ''
+
+setup(name="look-a-like", packages=['lal', ],
+      install_requires=REQUIREMENTS, version="0.0.0",
+      author="Edward Turner",
+      author_email="edward.turnerr@gmail.com",
+      description=README,
+      license=LICENSE
+      )
