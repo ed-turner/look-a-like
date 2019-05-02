1b3af1503c99366fa8e0e4c9d990eda5c8774cd2
Edward Turner <edward.turnerr@gmail.com>
2019-05-02 19:54:36 -0400

    added distance calculation function

--- scala-package/src/main/scala/com/lal/KNN.scala
+++ scala-package/src/main/scala/com/lal/KNN.scala
@@ -8,9 +8,20 @@ import org.apache.spark.sql.DataFrame
 
 abstract class KNN(k: Int, featureCol: String) {
 
-  type Sample = (BigInt, Vector)
+  type Sample = (BigInt, Vector[Double])
 
-  abstract def calculateSampleDistance(samp1: Sample, samp2: Sample): Double
+  def calculateSampleDistance(samp1: Sample, samp2: Sample): Double = {
+
+    val num_features: Int = samp1._2.size
+
+    def tmp_funct(i: Int, acc: Double): Double i match = {
+      case _ if i == num_features => acc
+      case _ => tmp_funct(i + 1, acc + Math.pow(samp1._2(i) - samp2._2(i), 2.0))
+    }
+
+    tmp_funct(0, 0.0)
+
+  }
 
   def getKNeighbors(sample1: Sample, samples: Stream[Sample]): Stream[BigInt] = {
     val distances: ParVector[(BigInt, Double)] = samples.par.map(_ =>(_._1, calculateSampleDistance(sample1, _)))
