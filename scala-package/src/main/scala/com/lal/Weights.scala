7a9425e42b32783f99d24899430980196d766a1e
Edward Turner <edward.turnerr@gmail.com>
2019-05-05 14:56:31 -0700

    code documentation for the weighing package.. the implementation is done.

--- scala-package/src/main/scala/com/lal/Weights.scala
+++ scala-package/src/main/scala/com/lal/Weights.scala
@@ -5,13 +5,24 @@ import org.apache.spark.sql.DataFrame
 import org.apache.spark.ml.linalg.DenseVector
 
 // this is for the validation split
+import org.apache.spark.ml.PredictionModel
 import org.apache.spark.ml.param.Param
 import org.apache.spark.ml.tuning.{ParamGridBuilder, TrainValidationSplit}
+import org.apache.spark.ml.evaulation.{Evaluator, BinaryClassificationEvaluator, RegressionEvaluator}
 
 // this is for the chosen model structure
 import org.apache.spark.ml.Estimator
-import org.apache.spark.ml.classification.GradientBoostingClassifier
-import org.apache.spark.ml.regression.GradientBoostingRegressor
+import org.apache.spark.ml.classification.GBTClassifier
+import org.apache.spark.ml.regression.GBTRegressor
+
+/**
+  * A trait to represent a ''feature weighting process''.
+  *
+  *
+  * @author Edward Turner
+  * @todo Add additional documentation about this trait
+  * @version 1.0
+  */
 
 // this is a private abstract class not for use on the user end side
 private trait Weights{
@@ -22,68 +33,100 @@ private trait Weights{
   // this paramgrid is define upon inheritance
   val paramGrid: Map[Param[_], Iterable[_]]
 
+  // the evaluator to use
+  val evaluator: Evaluator
+
   // this is the base model we want to use, which is defined upon inheritance
   val model: Estimator
 
-  def optModel(df: DataFrame): Estimator = {
+  /**
+    * @return Returns the optimized `model` of this weighting process.
+    */
+  def optModel(df: DataFrame): PredictionModel = {
     """
       | This function will optimize the Estimator chosen for this class
     """.stripMargin
-    // this is pseudo code to describe what I would like to do
 
-    // this should split the dataframe into two
-    // train_df, val_df = TrainValidationSplit().split(df)
+    val cv = new TrainValidationSplit()
+      .setEstimator(model)
+      .setEvaluator(evaluator)
+      .setEstimatorParamMaps(paramGrid)
+      .setTrainRatio(0.8)
+      .setParallelism(2)
 
-    // this should use the parameters, build
-    // CrossValidator(ParamGridBuilder.build(paramGrid)).fit(train_df, val_df)
-
-    // the optimized parameters are then parsed and set in the base model.
-
-    // model.setParams(optParams)
+    cv.fit(df)
 
   }
 
-  // this is dependent on the base model.. But this should return a DenseVector
+  /**
+    * @return Returns feature importance based on the weighting process
+    */
   def getFeatureWeights: DenseVector
 
 }
 
-// this class will use the GradientBoostingRegressor
-case class GBRegressorWeights(featureCol: String, predictionCol: String) extends Weights {
+/**
+  * A trait to represent a ''feature weighting process'' using gradient boosting process.
+  *
+  *
+  * @author Edward Turner
+  * @todo Add additional documentation about this trait
+  * @version 1.0
+  */
+private trait GBWeights {
 
-  val paramGrid = Map(
-    "learning_rate" -> Array((-15 to 15).map(x => Math.pow(10.0, x.toDouble))),
-    "n_estimators" -> Array((0 until 15).map(x => 100*(x + 1)))
-  )
+  val paramGrid = new ParamGridBuilder()
+    .addGrid(model.maxIter, Array((0 until 15).map(x => 100*(x + 1))))
+    .addGrid(model.maxDepth, Array((0 until 15))
+      .addGrid(model.stepSize, Array((-15 to 0).map(x => Math.pow(10.0, x.toDouble))))
+      .addGrid(model.subsamplingRate, Array((-15 to 0).map(x => Math.pow(10.0, x.toDouble))))
+      .build()
 
-  val model: GradientBoostingRegressor = GradientBoostingRegressor(featureCol=featureCol,
-    predictionCol=predictionCol)
-
-  def getFeatureWeights(df: DataFrame): DenseVector = {
-    val opt_model: GradientBoostingRegressor = this.optModel(df)
-
-    val fitted_model = opt_model.fit(df)
-
-    return fitted_model.feature_importances
-  }
-}
-
-// this class will use the GradientBoostingRegressor
-case class GBClassifierWeights(featureCol: String, predictionCol: String) extends Weights {
-
-  val paramGrid: Map[Param[_], Iterable[_]] = Map(
-    "learning_rate" -> Array((-15 to 15).map(x => Math.pow(10.0, x.toDouble))),
-    "n_estimators" -> Array((0 until 15).map(x => 100*(x + 1)))
-  )
-
-  val model: GradientBoostingClassifier = GradientBoostingClassifier(featureCol=featureCol,
-    predictionCol=predictionCol)
-
+  /**
+    * @return Returns feature importance based on the weighting process normalized to sum to one.
+    */
   def getFeatureWeights(df: DataFrame): DenseVector = {
     val opt_model = this.optModel(df)
 
     val fitted_model = opt_model.fit(df)
 
-    return fitted_model.feature_importances
+    return fitted_model.featureImportances
   }
+
+}
+
+
+/**
+  * A class to represent a ''feature weighting process'' using the Gradient-Boosting Tree regression task.
+  *
+  *
+  * @author Edward Turner
+  * @todo Add additional documentation about this class
+  * @version 1.0
+  */
+final case class GBRegressorWeights(featureCol: String, predictionCol: String) extends Weights with GBWeights {
+
+  val evaluator = new RegressionEvaluator
+
+  val model: GradientBoostingRegressor = GradientBoostingRegressor(featureCol=featureCol,
+    predictionCol=predictionCol)
+
+}
+
+
+/**
+  * A class to represent a ''feature weighting process'' using the Gradient-Boosting Tree classification task.
+  *
+  *
+  * @author Edward Turner
+  * @todo Add additional documentation about this class
+  * @version 1.0
+  */
+final case class GBClassifierWeights(featureCol: String, predictionCol: String) extends Weights with GBWeights {
+
+  val evaulator = new BinaryClassificationEvaluator
+
+  val model: GBTClassifier = GBTClassifier(featureCol=featureCol,
+    predictionCol=predictionCol)
+
 }
